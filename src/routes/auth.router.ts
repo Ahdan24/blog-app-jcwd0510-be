8da42909453f18dc3078e9407e8d40739b3ac163
Ahdan24 <Auvailable@gmail.com>
2024-12-09 15:37:26 +0700

    feat: forgot-pass

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import {
+  forgotPasswordController,
   loginController,
   registerController,
 } from "../controllers/auth.controller";
-import { validateLogin, validateRegister } from "../validators/auth.validator";
+import { validateForgotPassword, validateLogin, validateRegister } from "../validators/auth.validator";
 
 const router = Router();
 
 router.post("/register", validateRegister, registerController);
 router.post("/login", validateLogin, loginController);
+router.post("/login", validateForgotPassword, forgotPasswordController);
 
 export default router;
